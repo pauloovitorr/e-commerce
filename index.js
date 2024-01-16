59d8a53b368e8ef3ec3216b19345726fc226b5fa
Paulo Vitor <paulov.pv25@gmail.com>
2024-01-15 22:31:54 -0300

    Primerio consumo da API

---
+++ index.js
@@ -0,0 +1,38 @@
+fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((dados) => {
+        return dados.json()
+    })
+    .then((x) => {
+         pc = x.results
+
+        preenche(pc)
+    })
+
+
+
+function preenche(pcx) {
+
+    let divPai = document.querySelector('#produtos')
+
+    for(let chave in pcx){
+
+        let pc = pcx[chave]
+
+        let div = `
+        <div id="${pc.id}">
+            <p>Titulo: ${pc.title} </p>
+            <p>Condição: ${pc.condition}</p>
+            <p>Quantidade: ${pc.available_quantity}</p>
+            <p>Preço: ${pc.price}</p>
+        </div>`
+    
+        divPai.innerHTML += div
+    }
+   
+
+    
+
+}
+
+
+
