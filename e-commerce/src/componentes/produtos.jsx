a95b9a4643afc61a81af3342bdb79b4987a04961
pauloovitorr <paulov.pv50@gmail.com>
2024-01-18 13:36:30 -0300

    Ajuste dos componentes

--- e-commerce/src/componentes/produtos.jsx
+++ e-commerce/src/componentes/produtos.jsx
@@ -1,6 +1,40 @@
 export default function Produtos() {
 
-  let buscar = document.getElementById('buscas')
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=notbook')
+  .then((dados) => {
+      return dados.json()
+  })
+  .then((dados)=>{
+    preenche(dados.results)
+  })
+ 
+  function preenche(pcx) {
+
+    let divPai = document.querySelector('#produtos')
+
+    divPai.innerHTML =''
+    
+
+    for(let chave in pcx){
+
+        let pc = pcx[chave]
+
+        let div = `
+        <div class="card" id="${pc.id}">
+            <div>
+                <img src="${pc.thumbnail}" alt="${pc.title}">
+            </div>
+
+            <div>
+            <a href="${pc.permalink}"target="_blank"><span class="titulo">${pc.title}</span></a>
+                <p>${pc.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+            </div>
+        </div>`
+    
+        divPai.innerHTML += div
+    }
+}
+ 
 
 
   return (
