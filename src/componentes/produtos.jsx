56da3ab2408140a906e500ee17ee5ca2da0b61a0
Paulo Vitor <paulov.pv25@gmail.com>
2024-01-19 19:56:26 -0300

    Melhoria na lógica de buscas com useState e useEffect

--- src/componentes/produtos.jsx
+++ src/componentes/produtos.jsx
@@ -1,34 +1,49 @@
-import {useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export default function Produtos() {
 
   const [inicia, Setinicia] = useState('')
-  
-  
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=notbook')
-  .then((dados) => {
-      return dados.json()
-  })
-  .then((dados)=>{
-    
-     preenche(dados.results)
-    
-  })
+  const [busca, Setbusca] = useState('')
 
 
-  
+  useEffect(() => {
+
+    if (inicia == '') {
+      fetch('https://api.mercadolibre.com/sites/MLB/search?q=notbook')
+        .then((dados) => {
+          return dados.json()
+        })
+        .then((dados) => {
+
+          preenche(dados.results)
+        })
+    }
+    else {
+      fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${busca}`)
+        .then((dados) => {
+          return dados.json()
+        })
+        .then((dados) => {
+          preenche(dados.results)
+        })
+    }
+
+  }, [inicia])
+
+
+
   function preenche(pcx) {
 
     let divPai = document.querySelector('#produtos')
 
-    divPai.innerHTML =''
-    
+    divPai.innerHTML = ''
 
-    for(let chave in pcx){
 
-        let pc = pcx[chave]
+    for (let chave in pcx) {
 
-        let div = `
+      let pc = pcx[chave]
+
+      let div = `
         <div class="card" id="${pc.id}">
             <div>
                 <img src="${pc.thumbnail}" alt="${pc.title}">
@@ -36,51 +51,23 @@ export default function Produtos() {
 
             <div>
             <a href="${pc.permalink}"target="_blank"><span class="titulo">${pc.title}</span></a>
-                <p>${pc.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                <p>${pc.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
                 <button class="carrinho"> <i class="fa-solid fa-cart-plus"></i> </button>
             </div>
         </div>`
-    
-        divPai.innerHTML += div
+
+      divPai.innerHTML += div
     }
-}
- 
+  }
 
-function buscarProduto(){
-  let inputBusca = document.getElementById('buscas')
-
-  if(inputBusca.value!=''){
-
-    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${inputBusca.value}`)
-    .then((dados) => {
-        return dados.json()
-    })
-    .then((dados) => {
-      preenche(dados.results)
-    })
-}
-else{
-
-    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=notbook`)
-    .then((dados) => {
-        return dados.json()
-    })
-    .then((dados) => {
-         
-        preenche(dados.results)
-
-    })
-}
-
-}
 
 
   return (
     <main>
 
       <div className="buscar">
-        <input type="text" id="buscas" placeholder="Buscar produtos, marcas e muito mais..."/>
-        <img src="./lupa.jpg" alt="pesquisar" onClick={buscarProduto} />
+        <input type="text" id="buscas" placeholder="Buscar produtos, marcas e muito mais..." onChange={(e) => { Setbusca(e.target.value) }} />
+        <img src="./lupa.jpg" alt="pesquisar" onClick={() => { Setinicia(busca) }} />
       </div>
 
       <div id="produtos">
