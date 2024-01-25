383fa46ca694b01e975361d41b4c08daeee4677e
Paulo Vitor <paulov.pv25@gmail.com>
2024-01-25 20:13:50 -0300

    Incluindo local para adicionar o valor total

--- src/componentes/itensCarrinho.jsx
+++ src/componentes/itensCarrinho.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 export default function Itens_carrinho({ itens, produto }) {
     return (
 
-        <div>
+        <div className="container_carrinho">
 
-            <h1>Meu carrinho</h1>
+
             <div className="produtoss">
+
 
                 {itens.length > 0 ?
 
@@ -18,9 +19,9 @@ export default function Itens_carrinho({ itens, produto }) {
                             </div>
 
                             <div>
-                                <Link to='/produto' onClick={()=> {produto(item)}}><span class="titulo">{item.title}</span></Link>
+                                <Link to='/produto' onClick={() => { produto(item) }}><span class="titulo">{item.title}</span></Link>
                                 <p>{item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
-                                <button className="carrinho">Adicionar <i class="fa-solid fa-cart-plus"></i> </button>
+                                <button className="carrinho">Remover <i class="fa-solid fa-cart-plus"></i> </button>
                             </div>
                         </div>
                     ))
@@ -31,6 +32,19 @@ export default function Itens_carrinho({ itens, produto }) {
                 }
 
             </div>
+            {
+                itens.length > 0 ? <div className="preco">
+                    {
+                        itens.map((prod) => (
+                            <div className="conta">
+                                <img src={prod.thumbnail} alt={prod.title}></img>
+                                <p>{prod.title}</p>
+                                <p>{prod.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
+                            </div>
+                        ))
+                    }
+                </div> : ''
+            }
         </div>
     )
 }
