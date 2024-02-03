37bd849254a28720236486bdc52e7566782ab663
Paulo Vitor <paulov.pv25@gmail.com>
2024-02-03 10:53:26 -0300

    Melhoria no rodapé

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -3,8 +3,9 @@ export default function Rodape(){
 
         <footer>
             <p>Copyright 2024</p>
-            <p>Pojeto desenvolvido por Paulo Vitor</p>
+            <p>Autor do projeto: Paulo Vitor</p>
             <p><a href="https://www.linkedin.com/in/paulo-vitor-b98332253/" target="_blank">Linkedin do autor</a></p>
+            <p>Projeto desenvolvido com API do mercado Livre</p>
         </footer>
 
     )
