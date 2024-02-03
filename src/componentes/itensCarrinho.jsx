804a82caa4e5881b989ecced61e03f2b7c70a7ee
Paulo Vitor <paulov.pv25@gmail.com>
2024-02-03 17:53:28 -0300

    finalização rodapé

--- src/componentes/itensCarrinho.jsx
+++ src/componentes/itensCarrinho.jsx
@@ -60,6 +60,13 @@ itens.forEach(element => {
                 </div> : ''
             }
         </div>
+
+        <footer className="rodape">
+            <p>Copyright 2024</p>
+            <p>Autor do projeto: Paulo Vitor</p>
+            <p><a href="https://www.linkedin.com/in/paulo-vitor-b98332253/" target="_blank">Linkedin do autor</a></p>
+            <p>Projeto desenvolvido com API do mercado Livre</p>
+      </footer>
         </>
     )
 }
